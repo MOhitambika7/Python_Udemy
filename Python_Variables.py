7bb9b2e670125677e36fe5cdd1096883fd92d70f
MOhitambika7 <mohitambikanand7@gmail.com>
2025-01-25 10:32:22 +0545

    This is the code for variables in python.

---
+++ Python_Variables.py
@@ -0,0 +1,31 @@
+# Integer variable.
+age = 17
+print("Age: ", age)
+
+#Float variable.
+Temperature = 36.6
+print("Temperature: ", Temperature)
+
+#String variable.
+Name = "Alice"
+print("Name: ", Name)
+
+# Boolean variable.
+isStudent = True
+print("isStudent: ", isStudent)
+
+# List variable
+Fruits = ["Apple", "Banana", "Kiwi", "Watermelon"]
+print("Fruits: ", Fruits)
+
+# Tuple variable
+Coordinates = (10, 20)
+print("Coordinates: ", Coordinates)
+
+#Dictionary variables
+person = {"Name": "John", "Age": 18}
+print("Person: ", person)
+
+# Reassigning a variable
+age = 19
+print(age)
