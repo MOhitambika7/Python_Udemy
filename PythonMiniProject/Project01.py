06cae937fb8ea47873791d6942b392133fbe4ba7
MOhitambika7 <mohitambikanand7@gmail.com>
2025-01-26 11:13:35 +0545

    This is the mini project on percentage calculating calculator in python.

---
+++ PythonMiniProject/Project01.py
@@ -0,0 +1,24 @@
+# Program to calculate the percentage.
+def calculate_percentage(total, obtained):
+     if total > 0:
+         return(obtained/total)
+     else:
+         return None
+ 
+# Main program
+print("Welcome to ithe percentage calculating calculator program.")
+
+try:
+    total = float(input("Enter the total value:-"))
+    obtained = float(input("Enter the obatained value:- "))     
+    
+    percentage = calculate_percentage(total, obtained)
+    
+    if percentage is not None:
+        print(f"The obtained percentage is:- {percentage}")
+        
+    else:
+        print("Error: The value must be greater than 0.")
+        
+except ValueError:
+    print("Error: Plese enter the valid numeric value.")          
